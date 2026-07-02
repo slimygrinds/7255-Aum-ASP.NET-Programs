6d21169351b13ed7a27823cffc764997a34f965c
Aum Vachhani <aum.vachhani129527@marwadiuniversity.ac.in>
2026-07-02 19:58:16 +0530

    Add files via upload

---
+++ 1/WebForm1.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="First_WEB_1.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox><br />
+            <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox><br />
+            <hr />
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Button" />
+        </div>
+        <p>
+            &nbsp;</p>
+    </form>
+</body>
+</html>
---
+++ 1/WebForm2.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace First_WEB_1
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(TextBox1.Text);
+
+            string result = "";
+
+            for(int i = 1; i <= a; i++)
+            {
+                result += i + ", ";
+            }
+
+            TextBox2.Text = result.Substring(0, result.Length - 2);
+        }
+    }
+}
